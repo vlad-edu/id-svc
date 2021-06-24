3d2fce797418834e402fabc6c1fd73074c39018b
DeplorableSight <84678124+DeplorableSight@users.noreply.github.com>
2021-06-24 19:26:29 +0300

    Home page + image

---
+++ src/IdService.App/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdService.App.Controllers
+{
+    public partial class HomeController : Controller
+    {
+        [HttpGet]
+        [AllowAnonymous]
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
