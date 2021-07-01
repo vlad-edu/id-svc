ed961a67045c9a7a99fc6a4588d96b5758085f2e
DeplorableSight <84678124+DeplorableSight@users.noreply.github.com>
2021-07-01 18:34:46 +0300

    New Profile Page

--- src/IdService.App/ViewModels/Profile/ProfileModel.cs
+++ src/IdService.App/ViewModels/Profile/ProfileModel.cs
@@ -1,18 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
-using IdService.Core.Constants;
-
-namespace IdService.App.ViewModels.Profile
+﻿namespace IdService.App.ViewModels.Profile
 {
     public sealed class ProfileModel
     {
-        [Required(ErrorMessage = "Please enter new password.")]
-        [Display(Name = "New password")]
-        [RegularExpression(RegexConstants.Username, ErrorMessage = "Invalid password.")]
-        public string? NewPassword { get; set; }
+        public string? FirstName { get; set; }
 
-        [Display(Name = "Confirm password")]
-        [DataType(DataType.Password)]
-        [Compare(nameof(NewPassword), ErrorMessage = "The password and confirmation password do not match.")]
-        public string? ConfirmPassword { get; set; }
+        public string? LastName { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Phone { get; set; }
+
+        public string? Status { get; set; }
     }
 }
