c5544ea1dce0ded518d2c8e19f92eb7b647bc02f
DeplorableSight <84678124+DeplorableSight@users.noreply.github.com>
2021-07-06 18:26:50 +0300

    New "Change password" page

--- src/IdService.App/ViewModels/Account/ChangePasswordModel.cs
+++ src/IdService.App/ViewModels/Account/ChangePasswordModel.cs
@@ -4,7 +4,7 @@ using IdService.Core.Constants;
 
 namespace IdService.App.ViewModels.Account
 {
-    public sealed class PasswordModel
+    public sealed class ChangePasswordModel
     {
         [Required]
         [DisplayName("Old password")]
@@ -12,15 +12,15 @@ namespace IdService.App.ViewModels.Account
         public string? OldPassword { get; set; }
 
         [Required(ErrorMessage = "Please enter your password.")]
-        [DisplayName("Password")]
+        [DisplayName("New password")]
         [DataType(DataType.Password)]
         [StringLength(64, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 8)]
         [RegularExpression(RegexConstants.Password, ErrorMessage = "Invalid Password.")]
-        public string? Password { get; set; }
+        public string? NewPassword { get; set; }
 
-        [Display(Name = "Confirm password")]
+        [Display(Name = "Confirm new password")]
         [DataType(DataType.Password)]
-        [Compare(nameof(Password), ErrorMessage = "The password and confirmation password do not match.")]
+        [Compare(nameof(NewPassword), ErrorMessage = "The password and confirmation password do not match.")]
         public string? ConfirmPassword { get; set; }
     }
 }
