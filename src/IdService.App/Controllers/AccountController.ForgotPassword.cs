e214d8d66d5359c56e621c680f50b032421ffb75
DeplorableSight <84678124+DeplorableSight@users.noreply.github.com>
2021-07-07 18:59:38 +0300

    ForgotPassword page (#13)

---
+++ src/IdService.App/Controllers/AccountController.ForgotPassword.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using IdService.App.ViewModels.Account;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdService.App.Controllers
+{
+    /// <summary>
+    /// SignIn, SignOut.
+    /// </summary>
+    public partial class AccountController
+    {
+        [HttpGet]
+        public IActionResult ForgotPassword()
+        {
+            return View(new ForgotPasswordModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ForgotPassword([FromForm] ForgotPasswordModel model)
+        {
+            return View(model);
+        }
+    }
+}
