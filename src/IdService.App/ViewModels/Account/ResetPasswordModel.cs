1b51bd7ba664c99ed28a878392911af6f76a235d
DeplorableSight <84678124+DeplorableSight@users.noreply.github.com>
2021-07-09 18:07:44 +0300

    ForgotPagee + ResetPassword

---
+++ src/IdService.App/ViewModels/Account/ResetPasswordModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using IdService.Core.Constants;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdService.App.ViewModels.Account
+{
+    public sealed class ResetPasswordModel
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid Email.")]
+        [DisplayName("Email address")]
+        public string? Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [DisplayName("New Password")]
+        [RegularExpression(RegexConstants.Password, ErrorMessage = "Invalid Password.")]
+        public string? NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [DisplayName("Confirm Password")]
+        [Compare(nameof(NewPassword), ErrorMessage = "The password and confirmation password do not match.")]
+        public string? ConfirmPassword { get; set; }
+
+        [HiddenInput]
+        public string? Token { get; set; }
+
+        [HiddenInput]
+        public Guid Id { get; set; }
+    }
+}
