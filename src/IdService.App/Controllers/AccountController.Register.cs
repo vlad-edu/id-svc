841db2b550145f6a8dc96390282d07693c2520d8
DeplorableSight <84678124+DeplorableSight@users.noreply.github.com>
2021-07-12 20:29:06 +0300

    Email confirm

--- src/IdService.App/Controllers/AccountController.Register.cs
+++ src/IdService.App/Controllers/AccountController.Register.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using AspNetCore.ReCaptcha;
+using IdService.App.Infrastructure.Extensions;
 using IdService.App.ViewModels.Account;
+using IdService.App.ViewModels.Message;
 using IdService.Data.Model.Identity;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace IdService.App.Controllers
 {
@@ -36,7 +40,18 @@ namespace IdService.App.Controllers
             };
 
             var result = await _userManager.CreateAsync(user, model.Password);
-            if (result.Succeeded) return RedirectToAction("Login");
+            if (result.Succeeded)
+            {
+                user = await _userManager.FindByNameAsync(model.Username);
+                if (user == null) return NotFound();
+
+                var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                var callbackUrl = Url.ActionLink("ConfirmEmail", "Account", new { token = token.Base64Encode(), id = user.Id.ToString("N") });
+                await _emailService.SendConfirmEmailAsync(new Uri(HtmlEncoder.Default.Encode(callbackUrl)), user.Email, user.FirstName);
+                _logger.LogInformation("Confirm email link has been sent to user.");
+
+                return RedirectToAction("Index", "Home");
+            }
 
             foreach (var error in result.Errors)
             {
@@ -53,7 +68,24 @@ namespace IdService.App.Controllers
             if (id == Guid.Empty) return NotFound();
             if (token == null) return NotFound();
 
-            throw new NotImplementedException();
+            var user = await _userManager.FindByIdAsync(id.ToString());
+            if (user == null) return NotFound();
+
+            var result = await _userManager.ConfirmEmailAsync(user, token.Base64Decode());
+            if (!result.Succeeded)
+            {
+                _logger.LogWarning("Email confirm wasn't completed successfully. {errors}", result.Errors);
+                return NotFound();
+            }
+
+            _logger.LogInformation("Email confirm was completed successfully. {@user}", user);
+
+            return ShowAlert(
+                "Email confirm was completed successfully.",
+                MessageLevel.Success,
+                "Well done",
+                actionName: "OK",
+                actionUrl: Url.Action("Index", "Home"));
         }
     }
 }
