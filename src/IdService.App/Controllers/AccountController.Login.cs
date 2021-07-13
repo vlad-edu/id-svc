068ac2c26c6eadd0fa06022a9c7d817301697a29
DeplorableSight <84678124+DeplorableSight@users.noreply.github.com>
2021-07-13 19:23:46 +0300

    Login with 2Fa (#17)

--- src/IdService.App/Controllers/AccountController.Login.cs
+++ src/IdService.App/Controllers/AccountController.Login.cs
@@ -23,6 +23,7 @@ namespace IdService.App.Controllers
         }
 
         [HttpPost]
+        [AllowAnonymous]
         public async Task<IActionResult> Login([FromForm] LoginModel model)
         {
             if (model == null) throw new ArgumentNullException(nameof(model));
@@ -33,6 +34,7 @@ namespace IdService.App.Controllers
             }
 
             var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberLogin, true);
+            if (result.RequiresTwoFactor) return RedirectToAction("LoginWith2Fa");
             if (result.Succeeded)
             {
                 return Redirect(model.ReturnUrl ?? "/");
@@ -40,6 +42,40 @@ namespace IdService.App.Controllers
 
             ModelState.TryAddModelError(string.Empty, result.ToString());
             return View(model);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> LoginWith2Fa()
+        {
+            var user = await _signInManager.GetTwoFactorAuthenticationUserAsync();
+            if (user == null) return NotFound();
+
+            var model = new LoginWith2FaModel
+            {
+                ReturnUrl = "~/",
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> LoginWith2Fa([FromForm] LoginWith2FaModel model)
+        {
+            if (model == null) throw new ArgumentNullException(nameof(model));
+            var user = await _signInManager.GetTwoFactorAuthenticationUserAsync();
+            if (user == null) return NotFound();
+
+            if (!ModelState.IsValid) return View(model);
+
+            var result = await _signInManager.TwoFactorAuthenticatorSignInAsync(model.Code, true, model.RememberMachine);
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError(string.Empty, "The code is incorrect");
+                return View(model);
+            }
+
+            return RedirectToAction("Index", "Profile");
         }
     }
 }
