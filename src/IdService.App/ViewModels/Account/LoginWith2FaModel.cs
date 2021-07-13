3d2c4d3c8302b16f5ce4e3f1f7e119e933ad1653
DeplorableSight <84678124+DeplorableSight@users.noreply.github.com>
2021-07-13 19:21:02 +0300

    Login with 2Fa

---
+++ src/IdService.App/ViewModels/Account/LoginWith2FaModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdService.App.ViewModels.Account
+{
+    public class LoginWith2FaModel
+    {
+
+        [Required]
+        [DisplayName("Code")]
+        [RegularExpression(@"^\d{5,10}$", ErrorMessage = "Enter a 5 to 10 character code")]
+        public string? Code { get; set; }
+
+        [DisplayName("Remember me")]
+        public bool RememberMachine { get; set; }
+
+        [HiddenInput]
+        public string? ReturnUrl { get; set; }
+    }
+}
